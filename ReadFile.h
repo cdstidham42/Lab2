98d635282ca93a95a5dffa34535290bcc4e81f10
wkkizer42 <wkkizer42@students.tntech.edu>
2017-01-28 14:04:55 -0600

    Update ReadFile.h

--- ReadFile.h
+++ ReadFile.h
@@ -6,6 +6,36 @@
 #include <fstream>
 using namespace std;
 
+
+class ReadFile 
+{
+	private:
+		ifstream input_file;
+		bool _eof;
+		bool closed;
+	public:
+		ReadFile(const char* file_name);
+		~ReadFile();
+		void close();
+		String* readLine();
+		bool eof();
+		bool isClosed();
+
+};
+
+#endif
+
+
+// old struct
+/*
+#if !defined READ_FILE
+#define READ_FILE
+
+#include "Text.h"
+
+#include <fstream>
+using namespace std;
+
 struct ReadFile
 {
    ifstream input_file;
@@ -20,3 +50,4 @@ bool eof(ReadFile* rf);
 void close(ReadFile* rf);
 
 #endif
+*/
