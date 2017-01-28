d4ebba7f1b6cefb97adb0a9047acc958bd9ae5df
wkkizer42 <wkkizer42@students.tntech.edu>
2017-01-28 14:04:07 -0600

    Update ReadFile.cpp

--- ReadFile.cpp
+++ ReadFile.cpp
@@ -2,6 +2,66 @@
 #include <iostream>
 #include <string>
 
+
+///////////////////////////////
+//Wesley Kizer
+//CSC 2111 Lab 2
+
+ReadFile::ReadFile(const char* file_name)
+{
+   ReadFile* rf = new ReadFile;
+
+   rf->input_file.open(file_name);
+   rf->closed = false;
+   rf->_eof = false;
+
+   return rf;
+}
+
+ReadFile::~ReadFile()
+{
+   close(rf);
+   delete rf;
+}
+
+bool ReadFile::eof()
+{
+   return _eof;
+}
+
+void ReadFile::close()
+{
+   if (!closed)
+   {
+      input_file.close();
+      closed = true;
+   }
+}
+
+String* ReadFile::readLine()
+{
+   if (closed) return NULL;
+   if (_eof) return NULL;
+
+   string text;
+   _eof = !(getline(rf->input_file, text));
+
+   String* str = new String((const char*) text.c_str());
+   return str;
+}
+
+bool ReadFile::isClosed()
+{
+	return closed;
+]
+
+
+//Old struct
+/*
+#include "ReadFile.h"
+#include <iostream>
+#include <string>
+
 ReadFile* createReadFile(const char* file_name)
 {
    ReadFile* rf = new ReadFile;
@@ -44,3 +104,4 @@ String* readLine(ReadFile* rf)
    String* str = new String((const char*) text.c_str());
    return str;
 }
+*/
